006fab36489c7e8a3ba13cd18920fafbd69793a9
Kaloyan Daskalov <kdaskalov78@gmail.com>
2020-12-10 23:43:50 +0200

    adding form checks

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Name'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			street: {
 				elementType: 'input',
@@ -22,7 +27,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Street'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			zipCode: {
 				elementType: 'input',
@@ -30,7 +40,14 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'ZIP Code'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5
+				},
+				valid: false,
+				touched: false
 			},
 			country: {
 				elementType: 'input',
@@ -38,7 +55,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Country'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			email: {
 				elementType: 'input',
@@ -46,7 +68,12 @@ class ContactData extends Component {
 					type: 'email',
 					placeholder: 'Your E-Mail'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			deliveryMethod: {
 				elementType: 'select',
@@ -56,9 +83,15 @@ class ContactData extends Component {
 						{ value: 'normal', displayValue: 'Normal' }
 					]
 				},
-				value: ''
+				value: 'fast',
+				validation: {
+					required: false
+				},
+				valid: true,
+				touched: false
 			}
 		},
+		formIsValid: false,
 		loading: false
 	}
 
@@ -86,8 +119,37 @@ class ContactData extends Component {
 			);
 	}
 
+	checkValidity(value, rules) {
+		let isValid = true;
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.maxLength && isValid;
+		}
+
+		return isValid;
+	}
+
 	inputChangeHandler = (event, id) => {
-		this.setState((state) => state.orderForm[id].value = event.target.value);
+		this.setState((state) => {
+			state.orderForm[id].value = event.target.value;
+			state.orderForm[id].touched = true;
+			state.orderForm[id].valid = this.checkValidity(event.target.value, state.orderForm[id].validation);
+			let check = true;
+			for (const isValid in state.orderForm) {
+				check = state.orderForm[isValid].valid && check;
+			}
+			state.formIsValid = check;
+			// console.log(state.orderForm[id].valid);
+			return state;
+		});
 	}
 
 	render() {
@@ -103,9 +165,12 @@ class ContactData extends Component {
 					elementType={formElement.config.elementType}
 					elementConfig={formElement.config.elementConfig}
 					value={formElement.config.value}
+					invalid={!formElement.config.valid}
+					shouldValidate={formElement.config.validation.required}
+					touched={formElement.config.touched}
 					changed={(event) => this.inputChangeHandler(event, formElement.id)} />)
 			)}
-			<Button btnType="Success">ORDER</Button>
+			<Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
 		</form>);
 		if (this.state.loading) {
 			form = <Spinner />;
